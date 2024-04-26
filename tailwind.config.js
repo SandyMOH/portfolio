a797f7df3530551d44163f7bf67f3ebba49e88f2
sandymoh <sandymohammad99@gmail.com>
2024-04-26 15:07:16 +0700

    build: daisyui
    alo configure the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FE8609",
+          secondary: "#937DDB",
+          accent: "#FFFCE1",
+          neutral: "#FFFCE1",
+          "base-100": "#0E100F",
+          info: "#22d3ee",
+          success: "#a3e635",
+          warning: "#fde047",
+          error: "#E64120",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
 
